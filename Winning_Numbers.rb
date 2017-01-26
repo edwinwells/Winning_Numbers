bdbfd5ca5426169367d0ae9509fb6858610d0de1
edwinwells <edwin.wallace@yandex.com>
2017-01-26 10:51:35 -0500

    Basic Homework

--- Winning_Numbers.rb
+++ Winning_Numbers.rb
@@ -1,16 +1,8 @@
-def win()
-	puts "Congratulations! You win!"
-end
-
-def lose()
-	puts "Sorry, you did not win. Better luck next time."
-end
-
-
 
 def check_number(winning_numbers,user_number)
+
 	if winning_numbers.include?(user_number)
-		true
+		true 
 	else
 		false
 	end
